efa4a08fda954261b8b18aad164d9014f7b6379c
Oleg Bovykin <oleg.bovykin@gmail.com>
2012-12-28 01:34:30 +0400

    fix for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,10 @@ set :repository,  "git://github.com/arrowcircle/chef-test.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "192.168.2.160"                          # Your HTTP server, Apache/etc
-role :app, "192.168.2.160"                          # This may be the same as your `Web` server
-role :db,  "192.168.2.160", :primary => true # This is where Rails migrations will run
-role :db,  "192.168.2.160"
+role :web, "192.168.2.127"                          # Your HTTP server, Apache/etc
+role :app, "192.168.2.127"                          # This may be the same as your `Web` server
+role :db,  "192.168.2.127", :primary => true # This is where Rails migrations will run
+role :db,  "192.168.2.127"
 
 set :user, "webmaster"
 set :deploy_to, "/home/#{user}/projects/#{application}"
